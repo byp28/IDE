f1596e75f46b54a26228a038b134ceef512e8bd4
byp28 <yannight28@gmail.com>
2024-08-22 18:23:34 +0100

    22/08 18:24

---
+++ src/components/CodeTab.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function CodeTab() {
+
+    const tabsState = useSelector(state => state.tabs)
+    console.log(tabsState);
+
+  return (
+    <textarea spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
+
+    </textarea>
+  )
+}
