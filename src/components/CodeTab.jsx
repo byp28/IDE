a55364654fc27b51b6bc79bee755c44e18053c92
byp28 <yannight28@gmail.com>
2024-08-23 11:56:40 +0100

    23/08 11:08

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-export default function CodeTab() {
+export default function CodeTab({code,id}) {
 
-    const tabsState = useSelector(state => state.tabs)
-    console.log(tabsState);
+
 
   return (
-    <textarea spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
+    <textarea value={code} spellCheck="false" className='bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none'>
 
     </textarea>
   )
--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import CodeTab from './CodeTab'
+import { useSelector } from 'react-redux'
+
+import ButtonTab from './ButtonTab';
+import { useState } from 'react';
+
 
 export default function Tabs() {
-  return (
+
+    const tabs = useSelector(state => state.tabs)
+    console.log(tabs);
+    const [tabIndex,setTabIndex] = useState(tabs[0].id)
+
+    return (
     <div className='flex grow'>
         <div className='grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200'>
-            <button>HTML</button>
-            <button>CSS</button>
-            <button>JS</button>
+        {
+            tabs.map(tab =>(
+                <ButtonTab key={tab.id} 
+                id={tab.id} 
+                toggleTab={id => setTabIndex(id)} 
+                imgURL={tab.imgURL}
+                buttonContent={tab.buttonContent}
+                />
+            ))
+        }
+
         </div>
         <div className="w-full grow relative">
-            <CodeTab/>
+            <CodeTab
+                id={tabIndex}
+                code = {tabs.find(obj => obj.id === tabIndex).code}
+            />
         </div>
     </div>
   )
